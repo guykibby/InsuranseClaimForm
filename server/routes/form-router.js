b4d48cf0f036a5e06001d7d1d589093f62879c1a
Guy <guykibby@gmail.com>
2023-05-11 21:56:22 +1000

    Update form-router.js
    Co-Authored-By: Mitchellvari <103621930+Mitchellvari@users.noreply.github.com>
    Co-Authored-By: Ellaine <ellaine.simpkin@developersinstitute.co.nz>
    Co-Authored-By: Angus <89812891+azhishchenkov@users.noreply.github.com>

--- server/routes/form-router.js
+++ server/routes/form-router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const pool = require("../db");
 const formRouter = express.Router();
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
 
 formRouter.get("/", async (req, res) => {
   console.log("hot reloaded!");
@@ -12,19 +14,53 @@ formRouter.get("/", async (req, res) => {
   }
 });
 
+// Validation Schema
+
+const bodyVarSchema = Joi.object().keys({
+  policy_number: Joi.string()
+    .regex(/^\d{8}$/)
+    .required(),
+  customer_id: Joi.string().required(),
+  // policy_number: Joi.string().regex(/^\d{8}$/),
+  // customer_id: Joi.required(),
+  // condition_claimed_for: Joi.string().required(),
+  // first_symptoms_date: Joi.date().required(),
+  // symptoms_details: Joi.string().required(),
+  // medical_service_type: Joi.string().required(),
+  // service_provider_name: Joi.string().required(),
+  // other_insurance_provider: Joi.boolean().default(false),
+  // consent: Joi.boolean().valid(true).default(false),
+});
+
 // create a post route
 formRouter.post("/", async (req, res) => {
   try {
-    console.log(req.body);
+    // console log type of customer_id
+    console.log(typeof req.body.customer_id);
+    const {
+      policy_number,
+      customer_id,
+      condition_claimed_for,
+      first_symptoms_date,
+      symptoms_details,
+      medical_service_type,
+      service_provider_name,
+      other_insurance_provider,
+      consent,
+    } = req.body;
+
+    // console.log(typeof req.body, customer_id);
+    // bodyVarSchema.validate(request.params);
     // const { name, email, message } = req.body;
     const newItem = await pool.query(
       `INSERT INTO claims (policy_number, customer_id, condition_claimed_for,first_symptoms_date,symptoms_details,medical_service_type,service_provider_name,other_insurance_provider,consent)
-        VALUES ('${req.body.policy_number}', '${req.body.customer_id}','${req.body.condition_claimed_for}','${req.body.first_symptoms_date}','${req.body.symptoms_details}','${req.body.medical_service_type}','${req.body.service_provider_name}','${req.body.other_insurance_provider}','${req.body.consent}')`
+        VALUES ('${policy_number}', '${customer_id}','${condition_claimed_for}','${first_symptoms_date}','${symptoms_details}','${medical_service_type}','${service_provider_name}','${other_insurance_provider}','${consent}')`
     );
-
-    res.json("hello there");
+    print(newItem);
+    res.json(newItem.rows[0]);
   } catch (err) {
     console.error(err.message);
+    res.status(500).json({ error: err.message });
   }
 });
 
