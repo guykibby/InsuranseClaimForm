97d41a679c946dbd076cb162da42021afb628ae2
Guy <guykibby@gmail.com>
2023-05-11 21:04:55 +1000

    Update form-router.js
    Co-Authored-By: Mitchellvari <103621930+Mitchellvari@users.noreply.github.com>
    Co-Authored-By: Ellaine <ellaine.simpkin@developersinstitute.co.nz>
    Co-Authored-By: Angus <89812891+azhishchenkov@users.noreply.github.com>

--- server/routes/form-router.js
+++ server/routes/form-router.js
@@ -18,9 +18,9 @@ formRouter.post("/", async (req, res) => {
     console.log(req.body);
     // const { name, email, message } = req.body;
     const newItem = await pool.query(
-      `INSERT INTO Claims (policy_number, customer_id, condition_claimed_for,first_symptoms_date,symptoms_details,medical_service_type,service_provider_name,other_insurance_provider,consent)
-        VALUES ('$1','$2', '$3', '$4', '$5', '$6','$7','$8','$9'),[]`
-        );
+      `INSERT INTO claims (policy_number, customer_id, condition_claimed_for,first_symptoms_date,symptoms_details,medical_service_type,service_provider_name,other_insurance_provider,consent)
+        VALUES ('${req.body.policy_number}', '${req.body.customer_id}','${req.body.condition_claimed_for}','${req.body.first_symptoms_date}','${req.body.symptoms_details}','${req.body.medical_service_type}','${req.body.service_provider_name}','${req.body.other_insurance_provider}','${req.body.consent}')`
+    );
 
     //res.json("hello there");
   } catch (err) {
