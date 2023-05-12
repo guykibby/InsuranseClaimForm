e43184bf4b79b71db990a258ab668c05ac0a1013
Guy <guykibby@gmail.com>
2023-05-13 09:24:00 +1000

    Update InputForm.js
    Co-Authored-By: Mitchellvari <103621930+Mitchellvari@users.noreply.github.com>
    Co-Authored-By: Ellaine <ellaine.simpkin@developersinstitute.co.nz>
    Co-Authored-By: Angus <89812891+azhishchenkov@users.noreply.github.com>

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -63,87 +63,67 @@ const InputForm = () => {
     return <p>Oops, something went wrong!</p>;
   }
   return (
-    <form className="grid" onSubmit={onSubmit}>
-      <h1 className="text-center mt-5">Form</h1>
-
-      <label htmlFor="policyNumber">Policy Number</label>
-      <input
-        id="policyNumber"
-        type="text"
-        className="form-control"
-        value={policyNumber}
-        onChange={(e) => setPolicyNumber(e.target.value)}
-        required
-      />
-      <label htmlFor="customerId">Customer Id</label>
-      <input
-        id="customerId"
-        type="text"
-        className="form-control"
-        value={customerId}
-        onChange={(e) => setCustomerId(e.target.value)}
-        required
-      />
-      <label htmlFor="conditionClaimedFor">Condition Claimed For</label>
-      <input
-        id="conditionClaimedFor"
-        type="text"
-        className="form-control"
-        value={conditionClaimedFor}
-        onChange={(e) => setConditionClaimedFor(e.target.value)}
-        required
-      />
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        maxDate={new Date()}
-      />
-      <label htmlFor="symptomDetails">Symptom Details</label>
-      <input
-        id="symptomDetails"
-        type="text"
-        className="form-control"
-        value={symptomsDetails}
-        onChange={(e) => setSymptomsDetails(e.target.value)}
-        required
-      />
-      <label htmlFor="medicalServiceType">Medical Service Type</label>
-      <input
-        id="medicalServiceType"
-        type="text"
-        className="form-control"
-        value={medicalServiceType}
-        onChange={(e) => setMedicalServiceType(e.target.value)}
-        required
-      />
-      <label htmlFor="serviceProviderName">Service Provider Name</label>
-      <input
-        id="serviceProviderName"
-        type="text"
-        className="form-control"
-        value={serviceProviderName}
-        onChange={(e) => setServiceProviderName(e.target.value)}
-        required
-      />
-      <label htmlFor="otherInsuranceProvider">Other Insurance Provider</label>
-      <select
-        id="otherInsuranceProvider"
-        className="selectBox"
-        value={otherInsuranceProvider}
-        onChange={(e) => setOtherInsuranceProvider(e.target.value)}
-      >
-        <option value="true">Yes I have another insurance provider</option>
-        <option value="false">No enSure is my only insurance provider</option>
-      </select>
-      <div className="checkbox-wrapper">
-        <label>
-          <input
-            type="checkbox"
-            checked={isChecked}
-            onChange={() => setIsChecked((prev) => !prev)}
-            required
-          />
-          <span>I consent to the following</span>
+    <form>
+      <h1 className="text-center mt-5">Insurance Claims Form</h1>
+      <div className="claims-form">
+        <label htmlFor="policyNumber">Policy Number</label>
+        <input
+          id="policyNumber"
+          type="text"
+          className="form-control"
+          value={policyNumber}
+          onChange={(e) => setPolicyNumber(e.target.value)}
+        />
+        <label htmlFor="customerId">Customer Id</label>
+        <input
+          id="customerId"
+          type="text"
+          className="form-control"
+          value={customerId}
+          onChange={(e) => setCustomerId(e.target.value)}
+        />
+        <label htmlFor="conditionClaimedFor">Condition Claimed For</label>
+        <input
+          id="conditionClaimedFor"
+          type="text"
+          className="form-control"
+          value={conditionClaimedFor}
+          onChange={(e) => setConditionClaimedFor(e.target.value)}
+        />
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          maxDate={new Date()}
+        />
+        <label htmlFor="symptomDetails">Symptom Details</label>
+        <input
+          id="symptomDetails"
+          type="text"
+          className="form-control"
+          value={symptomsDetails}
+          onChange={(e) => setSymptomsDetails(e.target.value)}
+        />
+        <label htmlFor="medicalServiceType">Medical Service Type</label>
+        <input
+          id="medicalServiceType"
+          type="text"
+          className="form-control"
+          value={medicalServiceType}
+          onChange={(e) => setMedicalServiceType(e.target.value)}
+        />
+        <label htmlFor="serviceProviderName">Service Provider Name</label>
+        <input
+          id="serviceProviderName"
+          type="text"
+          className="form-control"
+          value={serviceProviderName}
+          onChange={(e) => setServiceProviderName(e.target.value)}
+        />
+        <label
+          htmlFor="otherInsuranceProvider"
+          className="otherInsuranceProvider"
+        >
+          Other Insurance Provider
         </label>
         <select
           id="otherInsuranceProvider"
@@ -177,11 +157,10 @@ const InputForm = () => {
             <span> I consent</span>
           </label>
         </div>
-        <button className="btn-success" onClick={onSubmit}>
-          Submit Form
+        <button className="btn btn-success" onClick={onSubmit}>
+          Submit
         </button>
       </div>
-      <input type="submit" />
     </form>
   );
 };
