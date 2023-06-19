5da62d51e59f8f3b4b304144ca2c1ac8eab18fd3
Guy <guykibby@gmail.com>
2023-06-19 14:36:34 +1000

    Privacy Policy

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,4 +1,5 @@
 import ensureSymbol from "../images/ensure-symbol.svg";
+import privacyStatement from "../images/PrivacyStatement.pdf";
 
 const footer = () => {
   return (
@@ -7,6 +8,7 @@ const footer = () => {
         <hr className="primary-divider" align="right"></hr>
         <img src={ensureSymbol} alt="enSURE Symbol" className="App-icon"></img>
         <p className="footerText">Keeping the unexpected, uncomplicated</p>
+        <a href={privacyStatement}>Privacy Statement</a>
       </footer>
     </div>
   );
